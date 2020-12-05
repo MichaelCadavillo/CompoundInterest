12048cd909b1b7ffa5c8f5b9e29484403c0adcf9
Michael Angelo Cadavillo <michael.cadavillo@gmail.com>
2020-12-06 00:02:24 +0800

    - Splitted code in different files for readability. - Added few sample TextFormFields

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/SlideUpPanel.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,6 +10,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    debugPaintSizeEnabled = false;
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
